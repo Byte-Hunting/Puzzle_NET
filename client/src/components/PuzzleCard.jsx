e8a244811fc50b851b386a7c44f0b4ac34cf1b3a
idris-1729 <mdidrissameer@gmail.com>
2025-08-20 21:12:51 +0530

    some css modification

--- client/src/components/PuzzleCard.jsx
+++ client/src/components/PuzzleCard.jsx
@@ -7,8 +7,8 @@ export default function PuzzleCard({ puzzle, onClick }) {
     <div className="puzzle-card" onClick={() => onClick(puzzle)} role="button">
       <MiniBoard fen={puzzle.fen} />
       <div style={{ padding: 10, fontSize: 13 }}>
-        <div style={{ fontWeight: 700 }}>{puzzle.id}</div>
-        <div style={{ marginTop: 6 }}>Rating: {puzzle.rating}</div>
+        <div style={{ fontWeight: 700, color: "#666" }}>{puzzle.id}</div>
+        <div style={{ marginTop: 6, color: "#666" }}>Rating: {puzzle.rating}</div>
         <div style={{ marginTop: 6, color: "#666", fontSize: 12 }}>
           Theme: {Array.isArray(puzzle.themes) ? puzzle.themes.join(", ") : puzzle.themes}
         </div>
